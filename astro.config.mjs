2a01b9865a2af67d7787227ecfb8badb0b462078
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-05-26 16:04:32 +0000

    basic layouting w/picocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: process.env.NETLIFY ? process.env.URL : 'http://localhost:3000',
+    compressHTML: true
+});
