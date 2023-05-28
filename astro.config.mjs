2439399c5d27dc2aeecf08788e2f7e3413cb0a35
radenpioneer <radenpioneer@users.noreply.github.com>
2023-05-29 00:01:01 +0700

    add sitemap and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig, sharpImageService } from 'astro/config'
 import mdx from '@astrojs/mdx'
-
 import react from '@astrojs/react'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.NETLIFY ? process.env.URL : 'http://localhost:3000',
   compressHTML: process.env.NETLIFY ? true : false,
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), sitemap()],
   experimental: {
     assets: true,
   },
