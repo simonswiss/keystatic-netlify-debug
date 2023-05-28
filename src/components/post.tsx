1a8ebc24aa8bdce89552dbd5c4fd0735b57a1a68
radenpioneer <radenpioneer@users.noreply.github.com>
2023-05-28 17:16:51 +0700

    implement post index

---
+++ src/components/post.tsx
@@ -0,0 +1,37 @@
+import type { FC } from 'react'
+import type { CollectionEntry } from 'astro:content'
+import style from '~/styles/post.module.scss'
+
+const Post: FC<{ post: CollectionEntry<'blog'> }> = ({ post }) => {
+  const url = [
+    post.data.date.getFullYear(),
+    post.data.date.getMonth() + 1,
+    post.slug,
+  ].join('/')
+
+  return (
+    <article>
+      <h2 className={style.title}>
+        <a href={`/blog/${url}`}>{post.data.title}</a>
+      </h2>
+      <p className={style.excerpt}>{post.body}</p>
+      <div className={style.tags}>
+        <a href={`/blog/${url}`}>
+          {post.data.date.toLocaleDateString('id-ID', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })}
+        </a>
+        {post.data.tags?.map((tag, i) => (
+          <a href={`#/tags/${tag}`} key={i}>
+            {tag}
+          </a>
+        ))}
+      </div>
+    </article>
+  )
+}
+
+export default Post
