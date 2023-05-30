d582532266f640ae2529155269b6e035da66fcc6
radenpioneer <radenpioneer@users.noreply.github.com>
2023-05-30 11:36:10 +0700

    add static pages collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,9 +24,20 @@ export const collections = {
     schema: ({ image }) =>
       z.object({
         title: z.string(),
+        description: z.string().optional(),
         date: z.date(),
         image: image().optional(),
         tags: z.array(z.string()).optional(),
+      }),
+  }),
+
+  page: defineCollection({
+    type: 'content',
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        image: image().optional(),
       }),
   }),
 }
