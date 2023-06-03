0b431a906c71e562ee432c304b73804c143c727b
radenpioneer <radenpioneer@users.noreply.github.com>
2023-06-03 23:52:14 +0700

    update samples and implement notes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,29 @@
 import { defineCollection, z } from 'astro:content'
 
 export const collections = {
+  blog: defineCollection({
+    type: 'content',
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        date: z.date(),
+        image: image().optional(),
+        tags: z.array(z.string()).optional(),
+        draft: z.boolean().default(false),
+        note: z.boolean().default(false),
+      }),
+  }),
+  page: defineCollection({
+    type: 'content',
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        image: image().optional(),
+        draft: z.boolean().default(false),
+      }),
+  }),
   site: defineCollection({
     type: 'data',
     schema: ({ image }) =>
@@ -16,28 +39,6 @@ export const collections = {
             })
             .optional()
         ),
-      }),
-  }),
-
-  blog: defineCollection({
-    type: 'content',
-    schema: ({ image }) =>
-      z.object({
-        title: z.string(),
-        description: z.string().optional(),
-        date: z.date(),
-        image: image().optional(),
-        tags: z.array(z.string()).optional(),
-      }),
-  }),
-
-  page: defineCollection({
-    type: 'content',
-    schema: ({ image }) =>
-      z.object({
-        title: z.string(),
-        description: z.string().optional(),
-        image: image().optional(),
       }),
   }),
 }
