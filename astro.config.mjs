2ae79ed27aa16bd17cdfbadd569d7aa2cc62ded6
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-06-11 22:56:04 +0000

    revamp layout and add markdoc support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 
+import markdoc from '@astrojs/markdoc'
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.NETLIFY ? process.env.URL : 'http://localhost:3000',
   compressHTML: process.env.NETLIFY ? true : false,
-  integrations: [mdx(), react(), sitemap()],
+  integrations: [mdx(), react(), sitemap(), markdoc()],
   experimental: {
     assets: true,
   },
