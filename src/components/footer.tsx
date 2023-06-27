e2a0ce23e55e5ad36b64d0b9d02a00e59bda7de1
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2023-06-28 06:28:02 +0700

    Update Netlify link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import 'external-svg-loader'
 import '../../node_modules/@picocss/pico/scss/components/_nav.scss'
 
 const NETLIFY_STATUS = {
-  url: new URL('https://app.netlify.com/sites/radenpioneer/deploys'),
+  url: new URL('https://www.netlify.com/'),
   badge: new URL(
     'https://api.netlify.com/api/v1/badges/9d6ccafc-6600-484f-a36d-daf284db2751/deploy-status'
   ),
@@ -14,7 +14,7 @@ const Footer: FC = () => (
   <nav className="container">
     <ul>
       <li>
-        <a href={NETLIFY_STATUS.url.href}>
+        <a href={NETLIFY_STATUS.url.href} rel="noreferrer">
           <svg data-src={NETLIFY_STATUS.badge.href} />
         </a>
       </li>
