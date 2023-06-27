92694c15eb0640ccfaac8d06d2702361acf6c7c6
R. Ilham <radenpioneer@users.noreply.github.com>
2023-06-28 01:26:22 +0700

    update keystatic config

--- src/keystatic/config.tsx
+++ src/keystatic/config.tsx
@@ -2,7 +2,11 @@ import { config, fields, singleton, collection } from '@keystatic/core'
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: {
+      owner: 'radenpioneer',
+      name: 'radenpioneer.blog'
+    }
   },
 
   singletons: {
