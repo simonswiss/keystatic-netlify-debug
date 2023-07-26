8ac44684160ef76dc73cdeb3feaefab4fd083589
R. Ilham <radenpioneer@users.noreply.github.com>
2023-07-26 07:54:46 +0700

    remove all images

--- src/keystatic/config.tsx
+++ src/keystatic/config.tsx
@@ -20,7 +20,7 @@ export default config({
         image: fields.image({
           label: 'Gambar',
           directory: 'src/assets/uploads/site',
-          publicPath: 'src/assets/uploads/site',
+          publicPath: '~/assets/uploads/site',
         }),
         menu: fields.array(fields.slug({ name: { label: 'Menu' } }), {
           label: 'Tautan',
@@ -42,7 +42,7 @@ export default config({
         image: fields.image({
           label: 'Gambar Cover',
           directory: 'src/assets/uploads/blog',
-          publicPath: 'src/assets/uploads/blog/',
+          publicPath: '~/assets/uploads/blog/',
         }),
         date: fields.date({ label: 'Tanggal Terbit' }),
         tags: fields.array(fields.slug({ name: { label: 'Tag' } }), {
@@ -58,7 +58,7 @@ export default config({
           tables: true,
           images: {
             directory: 'src/assets/uploads/blog',
-            publicPath: 'src/assets/uploads/blog/',
+            publicPath: '~/assets/uploads/blog/',
           },
         }),
       },
@@ -74,7 +74,7 @@ export default config({
         image: fields.image({
           label: 'Gambar Cover',
           directory: 'src/assets/uploads/page',
-          publicPath: 'src/assets/uploads/page/',
+          publicPath: '~/assets/uploads/page/',
         }),
         draft: fields.checkbox({ label: 'Draf', defaultValue: true }),
         content: fields.document({
@@ -85,7 +85,7 @@ export default config({
           tables: true,
           images: {
             directory: 'src/assets/uploads/page',
-            publicPath: 'src/assets/uploads/page/',
+            publicPath: '~/assets/uploads/page/',
           },
         }),
       },
